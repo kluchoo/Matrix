d1ec3037bd1d06ef3d2346e3b051c5e19eff4721
kluchoo <dominikzucho@gmail.com>
2024-12-10 22:13:46 +0100

    Edycja pliku hpp

---
+++ Matrix.hpp
@@ -0,0 +1,12 @@
+#ifndef A7F977BB_AE3A_48CD_A727_8AFC90710D10
+#define A7F977BB_AE3A_48CD_A727_8AFC90710D10
+
+class Matrix {
+    private:
+
+    public:
+    Matrix();
+    ~Matrix();
+};
+
+#endif /* A7F977BB_AE3A_48CD_A727_8AFC90710D10 */
