ae7d6ad2d5333912ad649bc3232ec7b8f4219d18
kluchoo <kluchoo@users.noreply.github.com>
2024-12-12 18:56:41 +0000

    deploy: a411e3ba397164789029388153386cf6071680c5

--- classMatrix.js
+++ classMatrix.js
@@ -2,6 +2,7 @@ var classMatrix =
 [
     [ "Matrix", "classMatrix.html#a2dba13c45127354c9f75ef576f49269b", null ],
     [ "Matrix", "classMatrix.html#a3637f6ad376fdc053d595bce3e17ec02", null ],
+    [ "Matrix", "classMatrix.html#af4c99503d4e4b31c5e73eceef9c7e1d0", null ],
     [ "~Matrix", "classMatrix.html#a7e496da93f0e3165a0dc82d79f7ba545", null ],
     [ "n", "classMatrix.html#a6f755dbfbefb9b4ba6c2b3acea8932f2", null ],
     [ "tab", "classMatrix.html#af45069dcccaf5064534287376925dde9", null ]
