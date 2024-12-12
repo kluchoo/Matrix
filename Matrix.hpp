632d1479b31222b0cbbbe0de02109e845a52d04e
kluchoo <dominikzucho@gmail.com>
2024-12-12 19:30:44 +0100

    Dodanie konstruktorów w pliku nagłówkowym

--- Matrix.hpp
+++ Matrix.hpp
@@ -6,7 +6,9 @@ class Matrix {
 
     public:
     Matrix();
-    ~Matrix();
+    Matrix(int n, int* t);
+    Matrix(Matrix& m);
+    ~Matrix(void);
 };
 
 #endif /* A7F977BB_AE3A_48CD_A727_8AFC90710D10 */
