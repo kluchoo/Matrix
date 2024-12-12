c98f927ad3808a64a70827bdaf3fd546f4e43bbc
kluchoo <dominikzucho@gmail.com>
2024-12-12 20:29:35 +0100

    testowanie funkcji

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main() {
     int t[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     Matrix m(3, t);
     Matrix m2(m);
+    m2.wstaw(1, 1, 0);
+    cout << m.pokaz(1, 1) << endl;
     cout << "test13\n";
     return 0;
 }
