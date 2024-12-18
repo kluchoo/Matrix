662419a9e36363b5f54fef40d3ed4bc7e529d495
Stygus <Stygus@users.noreply.github.com>
2024-12-18 22:44:57 +0000

    deploy: 745cdaaada68914dee84f52b27ba0026496ad02f

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['losuj_0',['losuj',['../classMatrix.html#a9890f7ceadc64dfce30c453bc766fbc1',1,'Matrix']]]
+  ['losuj_0',['losuj',['../classMatrix.html#a9890f7ceadc64dfce30c453bc766fbc1',1,'Matrix::losuj(void)'],['../classMatrix.html#a381bbe46892c80e0837e1080eea9943e',1,'Matrix::losuj(int x)']]]
 ];
