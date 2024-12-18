5099aa63704ac549fb0b0966da1d0bdb9e7348df
Stygus <Stygus@users.noreply.github.com>
2024-12-18 23:00:53 +0000

    deploy: 8581e051f852a5636ee8e4df61a57ccbcc729902

--- classMatrix.js
+++ classMatrix.js
@@ -6,6 +6,7 @@ var classMatrix =
     [ "~Matrix", "classMatrix.html#a7e496da93f0e3165a0dc82d79f7ba545", null ],
     [ "alokuj", "classMatrix.html#ab3f0a19323d5bd35dca008c68b99051d", null ],
     [ "diagonalna", "classMatrix.html#a58be50b5cc890341c14b1a9b370ab3ea", null ],
+    [ "diagonalna_k", "classMatrix.html#adc718e6ae5b2a8b3943a4e8c4bc53d9e", null ],
     [ "losuj", "classMatrix.html#a381bbe46892c80e0837e1080eea9943e", null ],
     [ "losuj", "classMatrix.html#a9890f7ceadc64dfce30c453bc766fbc1", null ],
     [ "odwroc", "classMatrix.html#a5b1bb742c2b57a692b7bfcc7310ea5b8", null ],
--- search/all_3.js
+++ search/all_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['diagonalna_0',['diagonalna',['../classMatrix.html#a58be50b5cc890341c14b1a9b370ab3ea',1,'Matrix']]]
+  ['diagonalna_0',['diagonalna',['../classMatrix.html#a58be50b5cc890341c14b1a9b370ab3ea',1,'Matrix']]],
+  ['diagonalna_5fk_1',['diagonalna_k',['../classMatrix.html#adc718e6ae5b2a8b3943a4e8c4bc53d9e',1,'Matrix']]]
 ];
