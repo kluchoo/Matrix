4ad486c3824516fc4cc176608ceac44926e2b959
Stygus <Stygus@users.noreply.github.com>
2024-12-18 23:05:22 +0000

    deploy: e973c8aefb7906820e44d4444c921a41afe50f08

--- classMatrix.js
+++ classMatrix.js
@@ -12,6 +12,7 @@ var classMatrix =
     [ "losuj", "classMatrix.html#a9890f7ceadc64dfce30c453bc766fbc1", null ],
     [ "odwroc", "classMatrix.html#a5b1bb742c2b57a692b7bfcc7310ea5b8", null ],
     [ "pokaz", "classMatrix.html#aed115facfa208a9530c4acde26ffe9b4", null ],
+    [ "wiersz", "classMatrix.html#ab248526b2a9757b3134c8f605699d2af", null ],
     [ "wstaw", "classMatrix.html#a4c51072cbc09f22715057260d901b746", null ],
     [ "n", "classMatrix.html#a6f755dbfbefb9b4ba6c2b3acea8932f2", null ],
     [ "tab", "classMatrix.html#af45069dcccaf5064534287376925dde9", null ]
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['wstaw_0',['wstaw',['../classMatrix.html#a4c51072cbc09f22715057260d901b746',1,'Matrix']]]
+  ['wiersz_0',['wiersz',['../classMatrix.html#ab248526b2a9757b3134c8f605699d2af',1,'Matrix']]],
+  ['wstaw_1',['wstaw',['../classMatrix.html#a4c51072cbc09f22715057260d901b746',1,'Matrix']]]
 ];
