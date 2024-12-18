6fd4facf239e934e8ca06e2960a66da0888d8a36
Stygus <Stygus@users.noreply.github.com>
2024-12-18 23:07:22 +0000

    deploy: 7aafb4d3961f9463810805b13bc912c090b5dae9

--- search/all_b.js
+++ search/all_b.js
@@ -3,5 +3,6 @@ var searchData=
   ['pokaz_0',['pokaz',['../classMatrix.html#aed115facfa208a9530c4acde26ffe9b4',1,'Matrix']]],
   ['private_1',['private',['../namespaceconvert.html#aaf142f8cbf0a54594b379e84402c1e5a',1,'convert']]],
   ['protected_2',['protected',['../namespaceconvert.html#a99a27ec9e9d1811bb61c761f313acec8',1,'convert']]],
-  ['public_3',['public',['../namespaceconvert.html#a688489b6f5e64d8d1723b593f2ff3524',1,'convert']]]
+  ['przekatna_3',['przekatna',['../classMatrix.html#a5a803eb1eddeeb51348addc3df4adfc4',1,'Matrix']]],
+  ['public_4',['public',['../namespaceconvert.html#a688489b6f5e64d8d1723b593f2ff3524',1,'convert']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['pokaz_0',['pokaz',['../classMatrix.html#aed115facfa208a9530c4acde26ffe9b4',1,'Matrix']]]
+  ['pokaz_0',['pokaz',['../classMatrix.html#aed115facfa208a9530c4acde26ffe9b4',1,'Matrix']]],
+  ['przekatna_1',['przekatna',['../classMatrix.html#a5a803eb1eddeeb51348addc3df4adfc4',1,'Matrix']]]
 ];
