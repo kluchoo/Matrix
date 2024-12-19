86f698a2f573724d47bddb50540c9012d85ec851
kluchoo <kluchoo@users.noreply.github.com>
2024-12-19 20:02:04 +0000

    deploy: c0393d3ed54b56327f5dfb2986d03033f20dc344

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['odwroc_0',['odwroc',['../classMatrix.html#a5b1bb742c2b57a692b7bfcc7310ea5b8',1,'Matrix']]]
+  ['odwroc_0',['odwroc',['../classMatrix.html#a5b1bb742c2b57a692b7bfcc7310ea5b8',1,'Matrix']]],
+  ['operator_2a_1',['operator*',['../classMatrix.html#ae1bc7e1fda06dab52353ac01b3196efd',1,'Matrix::operator*(Matrix &amp;m)'],['../classMatrix.html#ab0ea65b3aa311b3ebc018948d14ed7fe',1,'Matrix::operator*(int a)']]],
+  ['operator_2b_2',['operator+',['../classMatrix.html#a7cc7127ce96c997c9d14f1f8874ed742',1,'Matrix::operator+(Matrix &amp;m)'],['../classMatrix.html#aadabbccfbd0a95f43c8fe2f98a09b0c0',1,'Matrix::operator+(int a)']]],
+  ['operator_2d_3',['operator-',['../classMatrix.html#acd2e2721a17fcea4f41f3236f7869516',1,'Matrix']]]
 ];
