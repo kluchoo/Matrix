694e8b654779edd134999f598471b431c2134cd6
kluchoo <dominikzucho@gmail.com>
2024-12-19 21:15:29 +0100

    Operator*=

--- Matrix.cpp
+++ Matrix.cpp
@@ -373,3 +373,14 @@ Matrix& Matrix::operator-=(int a) {
     }
     return *this;
 }
+
+Matrix& Matrix::operator*=(int a) {
+    if (tab != nullptr) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tab[i][j] *= a; // Multiply all elements by a
+            }
+        }
+    }
+    return *this;
+}
